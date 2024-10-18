fb06a8c8504f9a677dbf43b4a2f5b8e67a04e8c6
Hijas2001 <hijas9089k@gmail.com>
2024-10-18 06:39:33 +0530

    revising codes anounimous function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-greet();  // Output: Hello!
-const greet = function() {
-    console.log("Hello!");
-  };
+// greet();  // Output: Hello!
+// const greet = function() {
+//     console.log("Hello!");
+//   };
   
+  
+// const greet = function() {
+//     console.log("Hello, World!");
+//   };
+  
+//   greet();  // Output: Hello, World!
+// setTimeout(function() {
+//     console.log("This message is delayed by 2 seconds.");
+//   }, 2000);
+// (function() {
+//     console.log("This runs immediately!");
+//   })();
   
